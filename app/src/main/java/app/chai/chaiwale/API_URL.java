8ed8ea3f533340bf11d1a9157ec03e214acb737e
nisha <nisharasiya13@gmail.com>
2019-03-16 16:55:22 +0530

    iudsykfds

--- app/src/main/java/app/chai/chaiwale/API_URL.java
+++ app/src/main/java/app/chai/chaiwale/API_URL.java
@@ -2,21 +2,21 @@ package app.chai.chaiwale;
 
 public class API_URL {
 
-  public static   String Login_URL="http://afsanaonline.com/chaiwala/app_api/sign_up.php";
-  public static   String GetProduct="http://afsanaonline.com/chaiwala/app_api/product_list.php";
-  public static   String getProfile="http://afsanaonline.com/chaiwala/app_api/update_profile.php";
-  public static   String getCart="http://afsanaonline.com/chaiwala/app_api/get_bucket.php";
-  public static   String AddBucket="http://afsanaonline.com/chaiwala/app_api/add_bucket.php";
-  public static   String Remove="http://afsanaonline.com/chaiwala/app_api/remove_bucket.php";
-  public static   String USeprofile="http://afsanaonline.com/chaiwala/app_api/view_profile.php";
-  public static   String orderproceed="http://afsanaonline.com/chaiwala/app_api/checkout.php";
-  public static   String orderHistory="http://afsanaonline.com/chaiwala/app_api/order_history.php";
-  public static   String getWallet="http://afsanaonline.com/chaiwala/app_api/get_wallet_ammount.php";
-  public static   String GetOrderId="http://afsanaonline.com/chaiwala/app_api/getorderid.php";
-  public static   String successPayment="http://afsanaonline.com/chaiwala/app_api/success_payment.php";
-  public static   String add_wallet_ammount="http://afsanaonline.com/chaiwala/app_api/add_wallet_ammount.php";
-  public static   String generate_opt="http://afsanaonline.com/chaiwala/app_api/generate_opt.php";
-  public static   String paybywallet="http://afsanaonline.com/chaiwala/app_api/paybywallet.php";
+  public static   String Login_URL="http://www.chaiwale.info/chaiwala/app_api/sign_up.php";
+  public static   String GetProduct="http://www.chaiwale.info/chaiwala/app_api/product_list.php";
+  public static   String getProfile="http://www.chaiwale.info/chaiwala/app_api/update_profile.php";
+  public static   String getCart="http://www.chaiwale.info/chaiwala/app_api/get_bucket.php";
+  public static   String AddBucket="http://www.chaiwale.info/chaiwala/app_api/add_bucket.php";
+  public static   String Remove="http://www.chaiwale.info/chaiwala/app_api/remove_bucket.php";
+  public static   String USeprofile="http://www.chaiwale.info/chaiwala/app_api/view_profile.php";
+  public static   String orderproceed="http://www.chaiwale.info/chaiwala/app_api/checkout.php";
+  public static   String orderHistory="http://www.chaiwale.info/chaiwala/app_api/order_history.php";
+  public static   String getWallet="http://www.chaiwale.info/chaiwala/app_api/get_wallet_ammount.php";
+  public static   String GetOrderId="http://www.chaiwale.info/chaiwala/app_api/getorderid.php";
+  public static   String successPayment="http://www.chaiwale.info/chaiwala/app_api/success_payment.php";
+  public static   String add_wallet_ammount="http://www.chaiwale.info/chaiwala/app_api/add_wallet_ammount.php";
+  public static   String generate_opt="http://www.chaiwale.info/chaiwala/app_api/generate_opt.php";
+  public static   String paybywallet="http://www.chaiwale.info/chaiwala/app_api/paybywallet.php";
 
 }
 
